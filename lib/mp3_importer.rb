2cb565f023ecab9a70c52d25beea754ecaa32c62
Kevin Downs <kevin.downs04@gmail.com>
2019-11-15 20:02:37 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,11 +4,11 @@ class MP3Importer
   attr_accessor :path
   
   def initialize(file_path)
-    @path = Dir.new(file_path)
+    @path = file_path
   end
   
   def files
-    file_list = Dir.glob('*.mp3')
+    file_list = Dir.new(@path).glob('*.mp3')
   end
   
   def import 
